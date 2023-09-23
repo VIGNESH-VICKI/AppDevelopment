83756ec276b92ebf1fb63b825e9de3759ef72e46
VIGNESH-VICKI <144884965+VIGNESH-VICKI@users.noreply.github.com>
2023-09-23 10:29:40 +0530

    Add files via upload

---
+++ Day 4/src/components/Dashboard.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../assets/css/Dashboard.css'
+const Dashboard = () => {
+  return (
+    <div>
+           
+   
+    <aside class="sidebar">
+        <nav>
+            <ul>
+                <li><a href="/">Dashboard</a></li>
+                <li><a href="/order">Orders</a></li>
+                <li><a href="/order">Cart</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/login">Login</a></li>
+                
+            </ul>
+        </nav>
+    </aside>
+
+    <main>
+    <section class="summary">
+           
+        </section>
+       
+        <section class="orders">
+           
+        </section>
+       
+        <section class="menu">
+           
+        </section>
+       
+        <section class="reservations">
+           
+        </section>
+       
+        <section class="settings">
+           
+        </section>
+    </main>
+
+    
+       
+    </div>
+         
+  )
+}
+
+export default Dashboard
