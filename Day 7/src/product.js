3aa86f4a3156b19f8475ed26cd74a9692a6943a6
VIGNESH-VICKI <144884965+VIGNESH-VICKI@users.noreply.github.com>
2023-09-25 16:31:39 +0530

    Add files via upload

---
+++ Day 7/src/product.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/shopping (7).webp";
+import product2 from "./assets/products/shopping (1).webp";
+import product3 from "./assets/products/shopping (2).webp";
+import product4 from "./assets/products/shopping (4).webp";
+import product5 from "./assets/products/shopping (5).webp";
+import product6 from "./assets/products/shopping (6).webp";
+import product7 from "./assets/products/shopping (8).webp";
+import product8 from "./assets/products/shopping (9).webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Remote Control Helihopter",
+    price: 50000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Trucks",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "hot wheels",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Barbie",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Beyblade",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Teddy Bear",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Water Gun",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Giggles",
+    price: 40.0,
+    productImage: product8,
+  },
+];
